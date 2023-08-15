8f0c2f8b9557472df0da59bdf6d3a72d67d8c105
DefyCab <love.bellinder@gmail.com>
2023-08-15 23:05:53 +0200

    subroutines

--- database.cob
+++ database.cob
@@ -1,3 +1,4 @@
+      >>SOURCE FORMAT FREE
        IDENTIFICATION DIVISION.
        PROGRAM-ID. database.
        AUTHOR. .
@@ -18,12 +19,26 @@
         01 HEJ pic x(10) value "Love".
         01 COUNTER pic 99 value ZEROS.
        PROCEDURE DIVISION.
-       Display "Hej" HEJ
-       Display COUNTER
-        PERFORM 5 TIMES ADD 1 TO COUNTER
-        DISPLAY COUNTER
-       end-perform
-       
-    
-             STOP RUN.
+    *>    Display "Hej" HEJ
+    *>    Display COUNTER
+    *>     PERFORM 5 TIMES ADD 1 TO COUNTER
+    *>     DISPLAY COUNTER
+    *>    end-perform
+       Subone.
+           display "Jag är i paragraf 1"
+           perform Subtwo
+           display "Jag är tillbaka i paragraf 1"
+           perform 5 times 
+           display "upprepa"
+           end-perform
+           STOP RUN.
+
+       Subthree.
+       display "Paragraf 3".
+
+       Subtwo. 
+       display "jag är i paragraf 2"
+       perform Subthree
+       Display "Jag är tillbaka i paragraf 2".
+             
    
