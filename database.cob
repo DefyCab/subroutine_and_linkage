412647bf49e24f6c643c07b47c92d3f81e1888a3
DefyCab <love.bellinder@gmail.com>
2023-08-17 06:12:10 +0200

    cleanin up unused fields

--- database.cob
+++ database.cob
@@ -1,32 +1,27 @@
       >>SOURCE FORMAT FREE
        IDENTIFICATION DIVISION.
        PROGRAM-ID. database.
-       AUTHOR. .
-       INSTALLATION.  where.
+       AUTHOR. Love Bellinder.
+       INSTALLATION.  local.
        DATE-WRITTEN.  14/08/2023.
        DATE-COMPILED. 14/08/2023.
        SECURITY.
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
-       SOURCE-COMPUTER. pc.
-       OBJECT-COMPUTER. pc.
+       SOURCE-COMPUTER. linux.
+       OBJECT-COMPUTER. linux.
        SPECIAL-NAMES.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
        DATA DIVISION.
         FILE SECTION.
         WORKING-STORAGE SECTION.
-        01 HEJ pic x(10) value "Love".
-        01 COUNTER pic 99 value ZEROS.
+        
         01 num1 pic 9 value 1.
         01 num2 pic 9 value 5.
         01 sum1 pic 9(2).
        PROCEDURE DIVISION.
-    *>    Display "Hej" HEJ
-    *>    Display COUNTER
-    *>     PERFORM 5 TIMES ADD 1 TO COUNTER
-    *>     DISPLAY COUNTER
-    *>    end-perform
+   
        Subone.
            display "Jag är i paragraf 1"
            perform Subtwo
@@ -42,7 +37,7 @@
        display "Paragraf 3".
 
        Subtwo. 
-       display "jag är i paragraf 2"
+       display "Jag är i paragraf 2"
        perform Subthree
        Display "Jag är tillbaka i paragraf 2".
              
@@ -53,3 +48,4 @@
        display num1 num2 sum1
 
        STOP RUN.
+        
